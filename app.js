3d867f46c3b8b1ed5017d146bb139919bd9cc389
Amir Gaffar <AmirG7@yahoo.com>
2021-01-31 20:28:03 -0500

    Compeleted both Exercise1 and Exercise2.

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
 console.log("Hello World!\n==========\n");
 
+
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+//While Loop
+let num = 1;
+
+while (num <= 100) {
+    if (num % 2 == 1) {
+        console.log(num);
+    }
+    num++;
+}
+ 
+//Do While Loop
+let num = 1;
+
+do {
+    if (num % 2 == 1) {
+        console.log(num);
+    }
+    num++;
+} while(num <= 100);
+
+//For Loop
+for (let num = 1; num <= 100; num++) {
+    if (num % 2 == 1){
+        console.log(num);
+    }
+}
+
+
+
+
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 === 0) {
+        console.log(`${i} FIZZ`);
+    } else if (i % 5 === 0) {
+        console.log(`${i} BUZZ`);
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        console.log (`${i} FIZZBUZZ`);
+    } else {
+        console.log(`${i} try again!`);
+    }
+}
+
